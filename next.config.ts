4f6e9fe461b76f81d259d7933bbd6c426234b727
Ibtesam-Hussain <ibtesamh786@gmail.com>
2026-09-06 21:57:02 +0500

    Configure Next.js with MDX support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
+
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+  options: {
+    // Note: rehype plugins can be added but need to be serializable
+    // For now, we'll use basic MDX without extra rehype plugins
+    // You can add syntax highlighting via CSS or other methods
+  },
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);
